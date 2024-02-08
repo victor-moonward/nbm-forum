97db519203b5c297894781b9e4290b42fce4a825
victor-moonward <victor@moonward.com.au>
2024-02-08 14:14:19 +1000

    feat: add colors and index to the theme folder

---
+++ src/styles/theme/colors.ts
@@ -0,0 +1,10 @@
+export const Colors = {
+  primary: '#6537FF',
+  text: '#383939',
+  clean: '#fff',
+  border: "#01021433",
+  placeholder: "#01021466",
+  inputIcon: "#383939",
+  warning: "#FF5757",
+  errorNotification: "#FA646433"
+};
