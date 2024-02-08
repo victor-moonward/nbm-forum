9f97d849637224d9fb04351cee6384fe7bd8455c
victor-moonward <victor@moonward.com.au>
2024-02-08 14:43:48 +1000

    feat: create login form component and constants file

---
+++ src/constants/validation.ts
@@ -0,0 +1,6 @@
+export const dictionaryMessage = {
+  GENERIC_REQUIRED_FIELD: 'Required field',
+  INVALID_EMAIL: 'Please enter a valid email address.',
+  MIN_CHARACTERS: 'Must be at least 8 characters',
+  INVALID_CREDENTIALS: 'Oops! Those details don’t seem to match. Check the details you entered are correct and try again.'
+};
