a068198ab6d47b8f836859d372d277d7a3c4b04b
victor-moonward <victor@moonward.com.au>
2024-02-09 10:32:20 +1000

    feat: refactor AuthBar component to add progress bar

--- src/components/auth/AuthBar.tsx
+++ src/components/auth/AuthBar.tsx
@@ -1,24 +1,58 @@
-import { Image, StyleSheet, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import { ArrowLeft } from "@/assets/icons/ArrowLeft";
-import { Title } from "@/components/common";
+import { IconButton, Title } from "@/components/common";
 import { Colors } from "@/styles/theme";
 import Logo from "@/assets/images/logo.png";
+import { useNavigation } from "@react-navigation/native";
+import { StackNavigation } from "@/types";
 
-export function AuthBar() {
+interface IAuthBar {
+  currentStep?: number;
+  totalSteps?: number;
+}
+
+export function AuthBar({ currentStep, totalSteps }: IAuthBar) {
+  const { navigate } = useNavigation<StackNavigation>();
+  const showProgressBar = currentStep !== undefined && totalSteps !== undefined;
+
   return (
-    <View>
-      <View style={styles.topBar}>
-        <ArrowLeft color={styles.icon.color} />
-        <View style={styles.logoContainer}>
-          <Title style={styles.title}>NBM</Title>
+    <View style={authBar.container}>
+      <View style={topBarStyles.container}>
+        <IconButton
+          onPress={() => navigate("Welcome")}
+          icon={
+            <ArrowLeft color={topBarStyles.icon.color} />
+          }
+        />
+        <View style={topBarStyles.logoContainer}>
+          <Title style={topBarStyles.title}>NBM</Title>
           <Image source={Logo} />
         </View>
       </View>
+      {showProgressBar && (
+        <View style={progressBarStyles.container}>
+          {new Array(totalSteps).fill(0).map((_, index) => (
+            <View
+              style={[
+                progressBarStyles.bar,
+                currentStep >= index ? progressBarStyles.activeBar : undefined
+              ]}
+            />
+          ))}
+        </View>
+      )}
     </View>
   )
 }
 
-const styles = StyleSheet.create({
+const authBar = StyleSheet.create({
+  container: {
+    gap: 20,
+    alignItems: 'center'
+  }
+});
+
+const topBarStyles = StyleSheet.create({
   logoContainer: {
     flexDirection: "row",
     flex: 1,
@@ -32,9 +66,25 @@ const styles = StyleSheet.create({
   icon: {
     color: Colors.text
   },
-  topBar: {
+  container: {
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
   }
-})
+});
+
+const progressBarStyles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    gap: 8,
+    maxWidth: '90%'
+  },
+  bar: {
+    flex: 1,
+    height: 8,
+    backgroundColor: Colors.border
+  },
+  activeBar: {
+    backgroundColor: Colors.primary
+  }
+});
