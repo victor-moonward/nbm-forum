ef43803233924e8ac7969444622c9b1c768f8733
victor-moonward <victor@moonward.com.au>
2024-02-13 09:39:49 +1000

    refactor: use useFormik custom hook instead of component in LoginForm

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,4 +1,4 @@
-import { Formik } from "formik";
+import { Formik, useFormik } from "formik";
 import { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { Button, IconButton, Input, Link, Text, Title } from "@/components/common";
@@ -11,101 +11,99 @@ import { useNavigation } from "@react-navigation/native";
 import { StackNavigation } from "@/types";
 
 export function LoginForm() {
-  const [hidePassword, setHidePassword] = useState(true);
+  const [isPasswordHidden, setIsPasswordHidden] = useState(true);
   const {
     handleFormSubmit,
-    formInitialValues,
     loginSchema,
     isLoading,
     errorMessage
   } = useLogin();
-  const { navigate } = useNavigation<StackNavigation>();
+  const {
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    values,
+    errors
+  } = useFormik({
+    initialValues: {
+      email: "",
+      password: ""
+    },
+    validationSchema: loginSchema,
+    onSubmit: handleFormSubmit,
+    validateOnBlur: false,
+    validateOnChange: false
+  })
 
   function handleShowPassword() {
-    setHidePassword(prev => !prev)
+    setIsPasswordHidden(prev => !prev)
   }
 
   return (
-    <Formik
-      initialValues={formInitialValues}
-      validationSchema={loginSchema}
-      onSubmit={handleFormSubmit}
-      validateOnChange={false}
-      validateOnBlur={false}
-    >
-      {({
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        values,
-        errors,
-      }) => (
-        <View style={styles.container}>
-          <View style={styles.textContainer}>
-            <Title>Log In</Title>
-            <Text>Enter your details to log into your account. </Text>
-          </View>
-          <View style={styles.inputsContainer}>
-            <Input
-              onChangeText={handleChange('email')}
-              onBlur={handleBlur('email')}
-              placeholder="you@email.com.au"
-              value={values.email}
-              label="Email"
-              error={errors.email}
-            />
-            <Input
-              onChangeText={handleChange('password')}
-              onBlur={handleBlur('password')}
-              placeholder="Enter your password"
-              value={values.password}
-              label="Password"
-              secureTextEntry={hidePassword}
-              error={errors.password}
-              icon={
-                <IconButton
-                  onPress={handleShowPassword}
-                  icon={hidePassword ?
-                    <VisibilityOff color={Colors.border} />
-                    :
-                    <VisibilityOn color={Colors.border} />
-                  }
-                />
+    <View style={styles.container}>
+      <View style={styles.textContainer}>
+        <Title>Log In</Title>
+        <Text>Enter your details to log into your account. </Text>
+      </View>
+      <View style={styles.inputsContainer}>
+        <Input
+          onChangeText={handleChange('email')}
+          onBlur={handleBlur('email')}
+          placeholder="you@email.com.au"
+          value={values.email}
+          label="Email"
+          error={errors.email}
+        />
+        <Input
+          onChangeText={handleChange('password')}
+          onBlur={handleBlur('password')}
+          placeholder="Enter your password"
+          value={values.password}
+          label="Password"
+          secureTextEntry={isPasswordHidden}
+          error={errors.password}
+          icon={
+            <IconButton
+              onPress={handleShowPassword}
+              icon={isPasswordHidden ?
+                <VisibilityOff color={Colors.border} />
+                :
+                <VisibilityOn color={Colors.border} />
               }
             />
-            <Link
-              onPress={() => console.log('test')}
-              style={styles.rightText}
-            >
-              Forgot your password?
-            </Link>
-          </View>
-          <Button
-            onPress={handleSubmit}
-            icon={<ArrowRight color={Colors.clean} />}
-            disabled={isLoading}
-          >
-            Log In
-          </Button>
-          <View style={styles.errorsContainer}>
-            {errorMessage && (
-              <Notification
-                type="error"
-                message={dictionaryMessage.INVALID_CREDENTIALS}
-              />
-            )}
-          </View>
-          <Link
-            onPress={() => navigate("CreateAccount")}
-            style={styles.centerText}
-          >
-            Don’t have an account?
-            {' '}
-            <Text style={styles.linkText}>Create one Here</Text>
-          </Link>
-        </View>
-      )}
-    </Formik>
+          }
+        />
+        <Link
+          onPress={() => console.log('test')}
+          style={styles.rightText}
+        >
+          Forgot your password?
+        </Link>
+      </View>
+      <Button
+        onPress={() => handleSubmit()}
+        icon={<ArrowRight color={Colors.clean} />}
+        disabled={isLoading}
+      >
+        Log In
+      </Button>
+      <View style={styles.errorsContainer}>
+        {errorMessage && (
+          <Notification
+            type="error"
+            message={dictionaryMessage.INVALID_CREDENTIALS}
+          />
+        )}
+      </View>
+      <Link
+        onPress={() => console.log('test')}
+        style={styles.centerText}
+      >
+        Don’t have an account?
+        {' '}
+        <Text style={styles.linkText}>Create one Here</Text>
+      </Link>
+    </View>
   )
 }
 
